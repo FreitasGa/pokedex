80af1b7b8fee6c4b6239080e9a526e7b2a7641ab
Gabriel Freitas <freitasga.01@gmail.com>
2022-06-08 12:59:37 -0300

    feat: Creating selectors

---
+++ src/selectors/pokemonsSelectors.ts
@@ -0,0 +1,14 @@
+import { createSelector } from 'reselect';
+
+import { RootState } from "../reducers";
+
+export const getPokemonsObject = (state: RootState) => state.pokemons.byId;
+export const getPokemonsIds = (state: RootState) => state.pokemons.allIds;
+export const getPokemonsIsLoading = (state: RootState) => state.pokemons.loading;
+export const getPokemonsLength = (state: RootState) => state.pokemons.allIds.length;
+export const getPokemonById = (state: RootState, id: number) => state.pokemons.byId[id];
+
+export const getPokemonsArray = createSelector(
+  getPokemonsObject,
+  (pokemonsObject) => Object.values(pokemonsObject)
+)
