997f4ba0ec12e1169fa0ca83311e12b079ec9ae3
Gabriel Freitas <freitasga.01@gmail.com>
2022-06-11 21:37:14 -0300

    fix: fix header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,9 @@ import {
   Group,
   Header as HeaderWrapper,
   Title,
+  useMantineTheme,
 } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { Home, User } from 'tabler-icons-react';
 
 import { useHeaderStyles } from '../styles/components';
@@ -26,6 +28,8 @@ const links = [
 
 export const Header = () => {
   const location = useLocation();
+  const theme = useMantineTheme();
+  const matches = useMediaQuery(`(max-width: ${theme.breakpoints.xs}px)`);
   const { classes } = useHeaderStyles();
 
   const [activeLink, setActiveLink] = useState<string>(location.pathname);
@@ -37,7 +41,7 @@ export const Header = () => {
       component={Link}
       to={link.pathname}
       onClick={() => setActiveLink(link.pathname)}
-      leftIcon={link.icon}
+      leftIcon={matches ? undefined : link.icon}
       variant={activeLink === link.pathname ? 'filled' : 'subtle'}
     >
       {link.name}
@@ -48,7 +52,7 @@ export const Header = () => {
     <HeaderWrapper className={classes.wrapper} height="fit-content">
       <Container className={classes.header} size="xl">
         <Title order={2}>Pokédex</Title>
-        <Group className={classes.linkGroup} spacing={5}>
+        <Group spacing={5}>
           {items}
         </Group>
       </Container>
--- src/styles/components/Header.ts
+++ src/styles/components/Header.ts
@@ -2,36 +2,22 @@ import { createStyles } from '@mantine/core';
 
 export const useHeaderStyles = createStyles((theme) => ({
   wrapper: {
-    position: 'relative',
-    zIndex: 1,
+    position: 'sticky',
+    top: 0,
+    zIndex: 10,
   },
   header: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    minHeight: '60px',
+    height: '60px',
 
     [theme.fn.smallerThan('xs')]: {
-      flexDirection: 'column',
-      height: 'fit-content',
       gap: '0.5rem',
-      padding: '1rem',
-    },
-  },
-  linkGroup: {
-    [theme.fn.smallerThan('xs')]: {
-      display: 'flex',
-      width: '100%',
-      justifyContent: 'space-between',
-      alignItems: 'center',
     },
   },
   link: {
     fontWeight: 600,
-
-    [theme.fn.smallerThan('xs')]: {
-      width: '100%',
-    },
   },
 }));
