9c2ec4728c19c94f1249aea162ad0943966ac9bc
Gabriel Freitas <freitasga.01@gmail.com>
2022-06-13 14:50:48 -0300

    fix: Adjustments on loader style

--- src/styles/pages/Collection.ts
+++ src/styles/pages/Collection.ts
@@ -1,20 +1,5 @@
 import { createStyles, keyframes } from '@mantine/core';
 
-const pokeball = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  25% {
-    transform: rotate(-15deg);
-  }
-  50% {
-    transform: rotate(15deg);
-  }
-  100% {
-    transform: rotate(0deg);
-  }
-`;
-
 const pokeballLoader = keyframes`
   0% {
     transform: rotate(0deg);
@@ -26,7 +11,9 @@ const pokeballLoader = keyframes`
 
 export const useCollectionStyles = createStyles(() => ({
   pokeball: {
-    animation: `${pokeball} 2s cubic-bezier(0.55, 0.06, 0.68, 0.19) infinite`,
+    color: 'transparent',
+    fill: 'transparent',
+    stroke: 'transparent',
   },
   pokeballLoading: {
     animation: `${pokeballLoader} 1s linear infinite`,
